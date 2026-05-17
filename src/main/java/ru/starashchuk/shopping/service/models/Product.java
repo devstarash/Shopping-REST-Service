65a462d25c5512ecc9adb71a625b099c1cdcfb61
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-17 16:20:44 +0800

    feat: Реализовал методы по работе с DAO

---
+++ src/main/java/ru/starashchuk/shopping/service/models/Product.java
@@ -0,0 +1,51 @@
+package ru.starashchuk.shopping.service.configs.models;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private int id;
+    private String name;
+    private Category category;
+    private BigDecimal price;
+    private int stock;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
