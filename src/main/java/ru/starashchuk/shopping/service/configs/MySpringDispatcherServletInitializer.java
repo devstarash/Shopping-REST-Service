c0613dbf7318e8917e061e7d4bd477c9c6410982
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-17 18:54:26 +0800

    feat: Добавил фильтр

--- src/main/java/ru/starashchuk/shopping/service/configs/MySpringDispatcherServletInitializer.java
+++ src/main/java/ru/starashchuk/shopping/service/configs/MySpringDispatcherServletInitializer.java
@@ -1,6 +1,8 @@
 package ru.starashchuk.shopping.service.configs;
 
+import jakarta.servlet.Filter;
 import org.jspecify.annotations.Nullable;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,5 +19,13 @@ public class MySpringDispatcherServletInitializer extends AbstractAnnotationConf
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters(){
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
     }
 }
