c6200121ea88ace6910473afaa713c933a3b3a92
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-17 16:23:30 +0800

    feat: Добавил часть ручек

--- src/main/java/ru/starashchuk/shopping/service/controllers/ProductController.java
+++ src/main/java/ru/starashchuk/shopping/service/controllers/ProductController.java
@@ -1,4 +1,24 @@
 package ru.starashchuk.shopping.service.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.starashchuk.shopping.service.models.Product;
+
+import java.util.List;
+
+@RestController("/products")
 public class ProductController {
+    @GetMapping
+    public List<Product> getProducts(){
+        return null;
+
+    }
+    @GetMapping("{id}")
+    public Product findById(@PathVariable int id){
+        return null;
+
+
+    }
+
 }
--- src/main/java/ru/starashchuk/shopping/service/controllers/PurchaseController.java
+++ src/main/java/ru/starashchuk/shopping/service/controllers/PurchaseController.java
@@ -1,4 +1,15 @@
 package ru.starashchuk.shopping.service.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.starashchuk.shopping.service.DTO.PurchaseRequestDTO;
+import ru.starashchuk.shopping.service.DTO.PurchaseResponseDTO;
+
+@RestController
 public class PurchaseController {
+    public PurchaseResponseDTO purchase(@RequestBody PurchaseRequestDTO requestDTO){
+        return null;
+
+    }
+
 }
