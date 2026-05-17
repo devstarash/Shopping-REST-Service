9668b9323b45e817c33963db5901f246a71fa553
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-17 17:24:14 +0800

    feat: Реализовал некоторые ручки

--- src/main/java/ru/starashchuk/shopping/service/controllers/ProductController.java
+++ src/main/java/ru/starashchuk/shopping/service/controllers/ProductController.java
@@ -1,24 +1,42 @@
 package ru.starashchuk.shopping.service.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.starashchuk.shopping.service.models.Product;
+import ru.starashchuk.shopping.service.servises.ProductService;
 
 import java.util.List;
 
 @RestController("/products")
 public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
-    public List<Product> getProducts(){
-        return null;
+    public List<Product> getProducts() {
+        return productService.findAll();
 
     }
+
     @GetMapping("{id}")
-    public Product findById(@PathVariable int id){
-        return null;
-
-
+    public Product findById(@PathVariable int id) {
+        return productService.findById(id);
     }
+
+    @GetMapping
+    public List<Product> findByCategoryId(@RequestParam Integer category) {
+        if (category == null) {
+            return productService.findAll();
+        }
+        return productService.findByCategoryId(category);
+    }
+
 
 }
--- src/main/java/ru/starashchuk/shopping/service/controllers/PurchaseController.java
+++ src/main/java/ru/starashchuk/shopping/service/controllers/PurchaseController.java
@@ -1,5 +1,6 @@
 package ru.starashchuk.shopping.service.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.starashchuk.shopping.service.DTO.PurchaseRequestDTO;
@@ -7,9 +8,15 @@ import ru.starashchuk.shopping.service.DTO.PurchaseResponseDTO;
 
 @RestController
 public class PurchaseController {
-    public PurchaseResponseDTO purchase(@RequestBody PurchaseRequestDTO requestDTO){
-        return null;
+    private PurchaseController purchaseController;
+    @Autowired
+    public PurchaseController(PurchaseController purchaseController) {
+        this.purchaseController = purchaseController;
+    }
 
+    public PurchaseResponseDTO purchase(@RequestBody PurchaseRequestDTO requestDTO){
+        PurchaseResponseDTO responseDTO = purchaseController.purchase(requestDTO);
+        return responseDTO;
     }
 
 }
