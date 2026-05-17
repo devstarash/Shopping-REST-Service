cd4775e4192bd017f725b4317ad6d1463096ac84
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-17 19:12:25 +0800

    feat: Добавил сканирование ресурсов

--- src/main/java/ru/starashchuk/shopping/service/configs/SpringConfig.java
+++ src/main/java/ru/starashchuk/shopping/service/configs/SpringConfig.java
@@ -2,12 +2,14 @@ package ru.starashchuk.shopping.service.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan("ru.starashchuk.shopping.service")
+@PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
     @Override
