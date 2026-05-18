5d161e6571ccee2895b7f860414238f69abb6cc0
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-05-18 09:30:21 +0800

    feat: Доабвил кастомную ошибку для DB

---
+++ src/main/java/ru/starashchuk/shopping/service/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package ru.starashchuk.shopping.service.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
